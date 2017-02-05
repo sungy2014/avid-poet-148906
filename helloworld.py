2725ffdef91e80595d63a4fd49c53995955ce610
sungy2014 <61529706@qq.com>
2017-02-05 17:51:37 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,19 @@
 import webapp2
+form = """
+<form action="/testform">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
-app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/testform', TestHandler)],
+                              debug=True)
