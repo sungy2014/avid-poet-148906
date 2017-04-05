959f0767c77ab31450ceeb1d096ee1e1516ae4b9
sungy2014 <61529706@qq.com>
2017-04-05 16:08:37 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,14 @@ import webapp2
 form = """
 <form>
     <label> Month
+    <input type="text" name="month">
+    </label>
+    <label> Day
+    <input type="text" name="day">
+    </label>
+    <label> Year
     <input type="text" name="year">
+    </label>
 </form>
 """
 class MainPage(webapp2.RequestHandler):
@@ -12,6 +19,7 @@ class MainPage(webapp2.RequestHandler):
         
     def post(selt):
         user_month = valid_month(self.response.get('month'))
+        pass
 
 # class TestHandler(webapp2.RequestHandler):
 #     def get(self):
