762d44346ba84b24626f55a48dfd71d2af29433b
sungy2014 <61529706@qq.com>
2017-04-05 16:05:54 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,22 @@
 import webapp2
 form = """
-<form action="/testform">
-    <input name="q">
-    <input type="submit">
+<form>
+    <label> Month
+    <input type="text" name="year">
 </form>
 """
 class MainPage(webapp2.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(form)
+        
+    def post(selt):
+        user_month = valid_month(self.response.get('month'))
 
-class TestHandler(webapp2.RequestHandler):
-    def get(self):
-        q = self.request.get("q")
-        self.response.out.write(q)
+# class TestHandler(webapp2.RequestHandler):
+#     def get(self):
+#         q = self.request.get("q")
+#         self.response.out.write(q)
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/testform', TestHandler)],
                               debug=True)
